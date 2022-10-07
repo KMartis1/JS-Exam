d27557edb85291aee92f34cc1abbd008ede74e20
KMartis1 <martynas.keturakis@gmail.com>
2022-10-07 16:29:15 +0300

    Aštunta užduotis - done

--- Task 8/script.js
+++ Task 8/script.js
@@ -1,7 +1,29 @@
 /* ------------------------------ TASK 8 --------------------------------------------
-Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri sukuria objektus su 3 metodais:
+Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5),
+kuri sukuria objektus su 3 metodais:
 sum() - priima du skaičius ir grąžina jų sumą.
 subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+    this.sum = function(a, b) {
+        return a + b
+    }
+    this.subtraction = function(a, b) {
+        return a - b
+    }
+    this.multiplication = function(a, b) {
+        return a * b
+    }
+    this.division = function(a, b) {
+        return a / b
+    }
+}
+
+// const numbers = new Calculator();
+// console.log(numbers.sum(10, 5));
+// console.log(numbers.subtraction(10, 5));
+// console.log(numbers.multiplication(10, 5));
+// console.log(numbers.division(10, 5));
